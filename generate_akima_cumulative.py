01d2434c94ef81535941d193e3621ccd8fca3dd2
rjadrich <rjadrich@gmail.com>
2017-06-27 15:02:16 -0500

    updated akima tool

--- generate_akima_cumulative.py
+++ generate_akima_cumulative.py
@@ -1,31 +1,49 @@
 import sys
 import json
-from numpy import arange, maximum, minimum
+from numpy import arange, maximum, minimum, array
+import re
 
 def GenerateAkimaCumulative(r_min, r_max, dr, d, epsilon=1.0, alpha=8, max_ur=float(1e8)):
-    if r_max <= d*(2.0**(1.0/alpha)):
-        raise Exception('The provided "r_max" is to short for generating an initial guess. Increase this by at least a little bit or decrease the input "d".')
     r_max = r_max + 1.0*dr
     r = arange(r_min, r_max+dr/2.0, dr)
     r = maximum(0.0001, r)
+    return ManualAkimaCumulative(r, d, epsilon, alpha, max_ur)
+
+def ManualAkimaCumulative(r, d, epsilon=1.0, alpha=8, max_ur=float(1e8)):
+    if max(r) <= d*(2.0**(1.0/alpha)):
+        raise Exception('The provided "r_max" is to short for generating an initial guess. Increase this by at least a little bit or decrease the input "d".')
     ur = (r <= d*(2.0**(1.0/alpha)))*(4.0*epsilon*((d/r)**(2*alpha) - (d/r)**(alpha)) + epsilon)
     ur = minimum(max_ur, ur)
     return zip(r, ur)
 
-if __name__ == "__main__":
-    #print 'Input format:'
-    #print '<spline_id> <positive_constraint>(true/false) <r_min> <r_max> <dr> <d>'
-    
+if __name__ == "__main__":    
     #get provided input
-    spline_id = sys.argv[1].strip()
-    positive_constraint = sys.argv[2].lower() == 'true'
-    r_min = float(sys.argv[3])
-    r_max =float(sys.argv[4])
-    dr = float(sys.argv[5])
-    d = float(sys.argv[6])
+    mode = sys.argv[1].strip()
+    spline_id = sys.argv[2].strip()
+    positive_constraint = sys.argv[3].lower() == 'true'
+    d = float(sys.argv[4])
+    if mode == 'gen_spacing':
+        r_min = float(sys.argv[5])
+        r_max =float(sys.argv[6])
+        dr = float(sys.argv[7])
     
-    #generate the data
-    r__ur = GenerateAkimaCumulative(r_min, r_max, dr, d)
+    #generate the data based on spcaing input or manual specification of not locations
+    if mode == 'gen_spacing':
+         r__ur = GenerateAkimaCumulative(r_min, r_max, dr, d)
+    elif mode:
+        f = open(mode, 'r')
+        data = f.readlines()
+        r = []
+        num_re = r'^\s*([0-9eE\+\-\.]+)\s*'
+        for line in data:
+            match = re.match(num_re, line)
+            if match:
+                r.append(float(match.group(1)))
+        f.close()
+        r = array(r)
+        r__ur = ManualAkimaCumulative(r, d)
+    else:
+         raise Exception('Invalid input provided for the mode. Enter "gen_spacing" or a filename that is not "gen_spacing".')
 
     #format the data
     params_val = {}
